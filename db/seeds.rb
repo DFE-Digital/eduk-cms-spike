4db2e5046a0b262acb8942b1e144224f4b1853a5
dankmitchell <dan@danmitchell.co.uk>
2019-05-26 08:42:52 +0100

    Add basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create an admin user
+Spina::User.create(name: 'admin', email: 'admin@education.gov.uk', password: 'password123', admin: true)
+
+# Create an 'Account' with the `Demo` themes
+Spina::Account.create(name: 'EDUK', theme: 'demo')
